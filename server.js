bbe983f3eb4933f780fba8a44cc5034bb9ad2546
Daniel Velazquez Giron <danip293@gmail.com>
2021-10-20 13:58:54 -0500

    .env-local and vercel.json files added

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const Task = require("./api/models/todoListModel"); //created model loading here
-const bodyParser = require("body-parser");
-const todoRouter = require("./api/routes/todoListRoutes"); //importing route
+const express = require('express');
+const mongoose = require('mongoose');
+const Task = require('./api/models/todoListModel'); //created model loading here
+const bodyParser = require('body-parser');
+const todoRouter = require('./api/routes/todoListRoutes'); //importing route
 
-const user = "danip293";
-const password = "lillos99";
-
+const MONGODB_URI = process.env.MONGODB_URI;
 const port = process.env.PORT || 3000;
 const app = express();
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose
-  .connect(
-    `mongodb+srv://${user}:${password}@cluster0.vcanv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-  )
+  .connect(MONGODB_URI)
   .then(() => {
-    console.log("connected to MongoDB");
+    console.log('connected to MongoDB');
   })
   .catch((err) => {
     console.error(err);
@@ -27,9 +23,9 @@ app.use(bodyParser.json());
 
 app.use(todoRouter); //register the route
 app.use(function (req, res) {
-  res.status(404).send({ url: req.originalUrl + " not found" });
+  res.status(404).send({ url: req.originalUrl + ' not found' });
 });
 
 app.listen(port);
 
-console.log("todo list RESTful API server started on: " + port);
+console.log('todo list RESTful API server started on: ' + port);
