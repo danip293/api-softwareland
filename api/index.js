be67eb452ad776d9126d4b34fb3337252f836146
Daniel Velazquez Giron <danip293@gmail.com>
2021-10-20 23:12:32 -0500

    index.js moved to api folder

---
+++ api/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Task = require('./models/todoListModel'); //created model loading here
+const bodyParser = require('body-parser');
+const todoRouter = require('./routes/todoListRoutes'); //importing route
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const port = process.env.PORT || 3000;
+const app = express();
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(todoRouter); //register the route
+app.use(function (req, res) {
+  res.status(404).send({ url: req.originalUrl + ' not found' });
+});
+
+app.listen(port);
+
+console.log('todo list RESTful API server started on: ' + port);
